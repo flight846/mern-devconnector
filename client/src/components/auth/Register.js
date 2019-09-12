111cdd31e0b8246cb4c84c22e1fef1b330e0670b
yazid <yazid@decision-science.com>
2019-09-12 22:31:15 +0800

    Add login component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,71 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
 
 const Register = () => {
+    const [ formData, setFormData ] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
+
+    const { name, email, password, password2 } = formData;
+
+    const onChange = e => setFormData({
+        ...formData,
+        [e.target.name]: e.target.value
+    })
+
+    const onSubmit = e => {
+        e.preventDefault()
+        if (password !== password2) {
+            console.log('Password do not match')
+        } else {
+            console.log(formData);
+        }
+    }
+
     return (
-        <div>
-            Register
-        </div>
+        <Fragment>
+            <h1 className="large text-primary">Sign Up</h1>
+            <p className="lead"><FaUser /> Create Your Account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input type="text" placeholder="Name" onChange={ e => onChange(e)} name="name" value={name} required />
+                </div>
+                <div className="form-group">
+                    <input type="email" placeholder="Email Address" onChange={e => onChange(e)} name="email" value={email} required/>
+                    <small className="form-text">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        onChange={e => onChange(e)}
+                        value={password}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="password2"
+                        minLength="6"
+                        onChange={e => onChange(e)}
+                        value={password2}
+                        required
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Register" />
+            </form>
+            <p className="my-1">
+                Already have an account? <Link to="/login">Sign In</Link>
+            </p>
+        </Fragment>
     )
 }
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
             <h1>
                 <Link to="/"><FaCode /> DevConnector</Link>
             </h1>
-            <ul class="nav">
+            <ul className="nav">
                 <li><NavLink to="profiles.html">Developers</NavLink></li>
                 <li><NavLink to="/register">Register</NavLink></li>
                 <li><NavLink to="/login">Login</NavLink></li>
