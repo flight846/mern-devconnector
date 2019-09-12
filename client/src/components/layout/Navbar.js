2f662b6cea42de4b2043a33acf1dd6937462a8f1
yazid <yazid@decision-science.com>
2019-09-12 21:40:31 +0800

    Add navbar links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
 import { FaCode } from 'react-icons/fa';
 
 const Navbar = () => {
     return (
         <nav className="navbar bg-dark">
             <h1>
-                <a href="index.html"><FaCode /> DevConnector</a>
+                <Link to="/"><FaCode /> DevConnector</Link>
             </h1>
-            <ul>
-                <li><a href="profiles.html">Developers</a></li>
-                <li><a href="register.html">Register</a></li>
-                <li><a href="login.html">Login</a></li>
+            <ul class="nav">
+                <li><NavLink to="profiles.html">Developers</NavLink></li>
+                <li><NavLink to="/register">Register</NavLink></li>
+                <li><NavLink to="/login">Login</NavLink></li>
             </ul>
         </nav>
     )
