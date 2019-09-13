d009b73f40d28faae6e878f770148cccbbabfa8a
yazid <yazid@decision-science.com>
2019-09-13 22:27:06 +0800

    Patch server

--- server.js
+++ server.js
@@ -1,38 +1,22 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const connectDB = require('./config/db');
 
 const app = express();
 
-// Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// Connect Database
+connectDB();
 
-// DB Config
-const db = require('./config/keys').mongoURI;
+// Init Middleware
+app.use(express.json({ extended: false }));
 
-// Connect to MongoDB
-mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+app.get('/', (req, res) => res.send('API Running'));
 
-// Passport middleware
-app.use(passport.initialize());
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-// Passport Config
-require('./config/passport')(passport);
+const PORT = process.env.PORT || 5000;
 
-// Use Routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
